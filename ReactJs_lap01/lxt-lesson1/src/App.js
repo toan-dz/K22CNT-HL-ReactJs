b2b191a9ae42c22179a97d56523df2c6173db031
toan-dz <ltoan5861@gmail.com>
2025-09-03 11:24:51 +0700

    lap01

--- ReactJs_lap01/lxt-lesson1/src/App.js
+++ ReactJs_lap01/lxt-lesson1/src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1>Lê Xuân Toàn</h1>
+      <hr />
+      <div className='alert alert-danger'>
+        <p>mã sinh viên : 2210900069</p>
+        <p>lớp : k22cnt2</p>
+        <p></p>
+        <p></p>
+      </div>
+
     </div>
   );
 }
